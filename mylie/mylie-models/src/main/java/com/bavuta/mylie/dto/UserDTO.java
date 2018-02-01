94923501bf7dc354edf475828142db404e4638f3
brutskov <brutskov@lendingclub.com>
2018-02-02 00:02:43 +0300

    add db and models

---
+++ mylie/mylie-models/src/main/java/com/bavuta/mylie/dto/UserDTO.java
@@ -0,0 +1,63 @@
+package com.bavuta.mylie.dto;
+
+import java.util.Date;
+
+import static com.bavuta.mylie.db.User.Gender;
+
+public class UserDTO extends AbstractDTO {
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private Date dateOfBirth;
+    private Gender gender;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
